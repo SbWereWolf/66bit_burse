83be3bb7c969a17ea2e2cf484eafe67c10463e9c
FORGE-JITA\ktokt <ulfnew@gmail.com>
2016-11-07 23:48:35 +0500

    + sketch mock-up

---
+++ Burse/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Burse.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var overview = new Models.BurseItemOverview {EventMessage = "Страница загружена"};
+            var result = View(overview);
+            return result;
+        }
+        public ActionResult AddBuyOrder()
+        {
+            var result = RedirectToAction("Index", "Home");
+            return result;
+        }
+        public ActionResult AddSellOrder()
+        {
+            var result = RedirectToAction("Index", "Home");
+            return result;
+        }
+    }
+}
---
+++ Burse/Models/BurseItemOverview.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Burse.Models
+{
+    public class BurseItemOverview
+    {
+        public Transaction[] Transactions { get; }
+        public BuyOrder[] BuyOrders { get; }
+        public SellOrder[] SellOrders { get; }
+
+        public string EventMessage { get; set; }
+
+        public BurseItemOverview()
+        {
+            var transactions = new List<Transaction>();
+            var buyOrders = new List<BuyOrder>();
+            var sellOrders = new List<SellOrder>();
+
+            transactions.Add(new Transaction
+            {
+                BuyComment = "WTB !!",
+                CompleteDate = new DateTime(42,01,23,0,0,6),
+                BuyDate = new DateTime(42, 01, 22,23,45,56),
+                Id = 1,
+                NumbersToTransaction = 12,
+                SellComment = "wts",
+                SellDate = new DateTime(42, 01, 22,23,59,59),
+                TransactionPrice = 20
+            });
+
+            buyOrders.Add(new BuyOrder
+            {
+                BuyDate = new DateTime(42, 01, 22, 23, 45, 56),
+                Id = 2,
+                BuyComment = "WTB !!",
+                BuyPrice = 25,
+                NumbersToBuy = 3
+            });
+
+            sellOrders.Add(new SellOrder
+            {
+                Id = 1,
+                SellComment = "wts",
+                SellDate = new DateTime(42, 01, 22, 23, 59, 59),
+                NumbersToSell = 1,
+                SellPrice = 20
+            });
+
+            transactions.Add(new Transaction
+            {
+                BuyComment = "WTB !!",
+                CompleteDate = new DateTime(42, 01, 23, 0, 0, 6),
+                BuyDate = new DateTime(42, 01, 22, 23, 45, 56),
+                Id = 1,
+                NumbersToTransaction = 12,
+                SellComment = "wts",
+                SellDate = new DateTime(42, 01, 22, 23, 59, 59),
+                TransactionPrice = 20
+            });
+
+            buyOrders.Add(new BuyOrder
+            {
+                BuyDate = new DateTime(42, 01, 22, 23, 45, 56),
+                Id = 2,
+                BuyComment = "WTB !!",
+                BuyPrice = 25,
+                NumbersToBuy = 3
+            });
+
+            sellOrders.Add(new SellOrder
+            {
+                Id = 1,
+                SellComment = "wts",
+                SellDate = new DateTime(42, 01, 22, 23, 59, 59),
+                NumbersToSell = 1,
+                SellPrice = 20
+            });
+
+
+            transactions.Add(new Transaction
+            {
+                BuyComment = "WTB !!",
+                CompleteDate = new DateTime(42, 01, 23, 0, 0, 6),
+                BuyDate = new DateTime(42, 01, 22, 23, 45, 56),
+                Id = 1,
+                NumbersToTransaction = 12,
+                SellComment = "wts",
+                SellDate = new DateTime(42, 01, 22, 23, 59, 59),
+                TransactionPrice = 20
+            });
+
+            buyOrders.Add(new BuyOrder
+            {
+                BuyDate = new DateTime(42, 01, 22, 23, 45, 56),
+                Id = 2,
+                BuyComment = "WTB !!",
+                BuyPrice = 25,
+                NumbersToBuy = 3
+            });
+
+            sellOrders.Add(new SellOrder
+            {
+                Id = 1,
+                SellComment = "wts",
+                SellDate = new DateTime(42, 01, 22, 23, 59, 59),
+                NumbersToSell = 1,
+                SellPrice = 20
+            });
+
+            transactions.Add(new Transaction
+            {
+                BuyComment = "WTB !!",
+                CompleteDate = new DateTime(42, 01, 23, 0, 0, 6),
+                BuyDate = new DateTime(42, 01, 22, 23, 45, 56),
+                Id = 1,
+                NumbersToTransaction = 12,
+                SellComment = "wts",
+                SellDate = new DateTime(42, 01, 22, 23, 59, 59),
+                TransactionPrice = 20
+            });
+
+            buyOrders.Add(new BuyOrder
+            {
+                BuyDate = new DateTime(42, 01, 22, 23, 45, 56),
+                Id = 2,
+                BuyComment = "WTB !!",
+                BuyPrice = 25,
+                NumbersToBuy = 3
+            });
+
+            sellOrders.Add(new SellOrder
+            {
+                Id = 1,
+                SellComment = "wts",
+                SellDate = new DateTime(42, 01, 22, 23, 59, 59),
+                NumbersToSell = 1,
+                SellPrice = 20
+            });
+
+            transactions.Add(new Transaction
+            {
+                BuyComment = "WTB !!",
+                CompleteDate = new DateTime(42, 01, 23, 0, 0, 6),
+                BuyDate = new DateTime(42, 01, 22, 23, 45, 56),
+                Id = 1,
+                NumbersToTransaction = 12,
+                SellComment = "wts",
+                SellDate = new DateTime(42, 01, 22, 23, 59, 59),
+                TransactionPrice = 20
+            });
+
+            buyOrders.Add(new BuyOrder
+            {
+                BuyDate = new DateTime(42, 01, 22, 23, 45, 56),
+                Id = 2,
+                BuyComment = "WTB !!",
+                BuyPrice = 25,
+                NumbersToBuy = 3
+            });
+
+            sellOrders.Add(new SellOrder
+            {
+                Id = 1,
+                SellComment = "wts",
+                SellDate = new DateTime(42, 01, 22, 23, 59, 59),
+                NumbersToSell = 1,
+                SellPrice = 20
+            });
+
+
+            sellOrders.Add(new SellOrder
+            {
+                Id = 1,
+                SellComment = "wts",
+                SellDate = new DateTime(42, 01, 22, 23, 59, 59),
+                NumbersToSell = 1,
+                SellPrice = 20
+            });
+
+            sellOrders.Add(new SellOrder
+            {
+                Id = 1,
+                SellComment = "wts",
+                SellDate = new DateTime(42, 01, 22, 23, 59, 59),
+                NumbersToSell = 1,
+                SellPrice = 20
+            });
+
+            Transactions = transactions.ToArray();
+            BuyOrders = buyOrders.ToArray();
+            SellOrders = sellOrders.ToArray();
+        }
+    }
+}
